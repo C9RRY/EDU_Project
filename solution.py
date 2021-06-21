d9634baae9adecc3638cff706b7a5eeb4e1b8aa6
C9RRY <66624951+C9RRY@users.noreply.github.com>
2021-06-21 12:16:15 +0300

    Initial commit

---
+++ solution.py
@@ -0,0 +1,34 @@
+import csv
+
+
+class CarBase:
+    def __init__(self, brand, photo_file_name, carrying):
+        pass
+
+
+class Car(CarBase):
+    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
+        pass
+
+
+class Truck(CarBase):
+    def __init__(self, brand, photo_file_name, carrying, body_whl):
+        pass
+
+
+class SpecMachine(CarBase):
+    def __init__(self, brand, photo_file_name, carrying, extra):
+        pass
+
+
+def get_car_list(csv_filename):
+    car_list = []
+    with open(f'{csv_filename}.csv', encoding='utf-8') as csv_fd:
+        reader = csv.reader(csv_fd, delimiter=';')
+        next(reader)  # пропускаем заголовок
+        for row in reader:
+            car_list.append(row)
+    return car_list
+
+print(get_car_list('coursera_week3_cars'))
+

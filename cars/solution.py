bb4235164f4daec285b360db2d25aa5e0689542c
C9RRY <66624951+C9RRY@users.noreply.github.com>
2021-06-21 17:22:52 +0300

    Initial commit

--- cars/solution.py
+++ cars/solution.py
@@ -3,33 +3,49 @@ import os
 
 
 class CarBase:
-    def __init__(self, brand=1, photo_file_name="", carrying=1):
+    def __init__(self, brand, photo_file_name, carrying):
         self.brand = brand
         self.photo_file_name = photo_file_name
         self.carrying = carrying
 
     def get_photo_file_ext(self):
-        return os.path.splitext(self.photo_file_name)
+        return os.path.splitext(self.photo_file_name)[1]
+
 
 
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
         super().__init__(brand, photo_file_name, carrying)
+        self.car_type = 'car'
 
 
 class Truck(CarBase):
     def __init__(self, brand, photo_file_name, carrying, body_whl):
         super().__init__(brand, photo_file_name, carrying)
+        self.car_type = 'truck'
+        self.body_whl = body_whl
+        try:
+            self.body_length = float(body_whl.split('x')[0])
+            self.body_width = float(body_whl.split('x')[1])
+            self.body_height = float(body_whl.split('x')[2])
+        except AttributeError:
+            self.body_length = 0
+            self.body_width = 0
+            self.body_height = 0
+
+
+
+
 
 
 class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
         super().__init__(brand, photo_file_name, carrying)
-
+        self.car_type = 'spec_machine'
 
 def get_car_list(csv_filename):
     car_list = []
-    with open(f'{csv_filename}.csv', encoding='utf-8') as csv_fd:
+    with open(f'{csv_filename}', encoding='utf-8') as csv_fd:
         reader = csv.reader(csv_fd, delimiter=';')
         next(reader)  # пропускаем заголовок
         for row in reader:
@@ -37,6 +53,8 @@ def get_car_list(csv_filename):
     return car_list
 
 
-first = CarBase()
+truck = Truck('Nissan', 'nissan.jpeg', '1.5', '3.92x2.09x1.87')
+print(truck.car_type, truck.brand, truck.photo_file_name, truck.body_length,
+      truck.body_width, truck.body_height, sep='\n')
 
 

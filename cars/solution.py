385e405c34430fc8e963ab1943bd583f69f26595
C9RRY <66624951+C9RRY@users.noreply.github.com>
2021-06-21 18:39:50 +0300

    Initial commit

--- cars/solution.py
+++ cars/solution.py
@@ -12,11 +12,11 @@ class CarBase:
         return os.path.splitext(self.photo_file_name)[1]
 
 
-
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
         super().__init__(brand, photo_file_name, carrying)
         self.car_type = 'car'
+        self.passenger_seats_count = passenger_seats_count
 
 
 class Truck(CarBase):
@@ -28,33 +28,44 @@ class Truck(CarBase):
             self.body_length = float(body_whl.split('x')[0])
             self.body_width = float(body_whl.split('x')[1])
             self.body_height = float(body_whl.split('x')[2])
-        except AttributeError:
+        except (AttributeError, ValueError):
             self.body_length = 0
             self.body_width = 0
             self.body_height = 0
 
-
-
-
+    def get_body_volume(self):
+        return self.body_height * self.body_length * self.body_width
 
 
 class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
         super().__init__(brand, photo_file_name, carrying)
         self.car_type = 'spec_machine'
+        self.extra = extra
+
 
 def get_car_list(csv_filename):
     car_list = []
     with open(f'{csv_filename}', encoding='utf-8') as csv_fd:
+        cars = []
         reader = csv.reader(csv_fd, delimiter=';')
         next(reader)  # пропускаем заголовок
         for row in reader:
-            car_list.append(row)
-    return car_list
+            try:
+                if row[0] == 'car':
+                    machine = Car(row[1], row[3], row[5], row[2])
+                elif row[0] == 'truck':
+                    machine = Truck(row[1], row[3], row[5], row[4])
+                elif row[0] == 'spec_machine':
+                    machine = SpecMachine(row[1], row[3], row[5], row[6])
+                cars.append(machine)
+            except IndexError:
+                del machine
+
+    return cars
 
 
-truck = Truck('Nissan', 'nissan.jpeg', '1.5', '3.92x2.09x1.87')
-print(truck.car_type, truck.brand, truck.photo_file_name, truck.body_length,
-      truck.body_width, truck.body_height, sep='\n')
-
-
+cars = get_car_list('coursera_week3_cars.csv')
+print(len(cars))
+for car in cars:
+    print(type(car))

07862c9ce5a00ec269f05d265699440741deef54
C9RRY <66624951+C9RRY@users.noreply.github.com>
2021-06-22 19:58:51 +0300

    Initial commit

--- magic_methods/solution.py
+++ magic_methods/solution.py
@@ -2,11 +2,23 @@ import os
 import tempfile
 
 
+
 class File:
+
     def __init__(self, name):
-        self.temp_dir = os.path.join(tempfile.gettempdir(), f'{name}')
-        with open(self.temp_dir, 'w', encoding='utf-8') as fn:
-            self.file = fn
+        self.temp_dir = os.path.join(f"{name}")
+        self.file = File.file_exist(self)
+
+    def file_exist(self):
+        if os.path.exists(self.temp_dir):
+            with open(self.temp_dir, 'w', encoding='utf-8') as fl:
+                return fl
+        else:
+            open(self.temp_dir, 'w', encoding='utf-8')
+            return ()
+
+    def read(self):
+        return self.file
 
 
 
@@ -16,6 +28,7 @@ path_to_file = "some_filename"
 print(os.path.exists(path_to_file))
 file_obj = File(path_to_file)
 print(os.path.exists(path_to_file))
+print(file_obj.read())
 
 
 

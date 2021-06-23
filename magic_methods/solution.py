559a6602a332069380129ee9ed4e2b5822a6a998
C9RRY <66624951+C9RRY@users.noreply.github.com>
2021-06-23 11:05:43 +0300

    Initial commit

--- magic_methods/solution.py
+++ magic_methods/solution.py
@@ -10,17 +10,28 @@ class File:
 
     def __add__(self, other):
         added_string = self.read() + other.read()
-        dir_of_file = os.path.join(tempfile.gettempdir(), self.file_dir)
-        added_file = File(dir_of_file)
+        added_file = File(self.file_dir)
+        added_file.create_tmp()
         added_file.write(added_string)
         return added_file
 
     def __iter__(self):
+        self.iter_list = []
+        for i in self.file.splitlines():
+            self.iter_list.append(i + '\n')
+        self.iter_count = len(self.iter_list)
         return self
 
     def __next__(self):
-        x =
-        return 2
+        if self.iter_count > 0:
+            out = self.iter_list[len(self.iter_list) - self.iter_count]
+            self.iter_count -= 1
+            return out
+        else:
+            raise StopIteration
+
+    def __str__(self):
+        return self.file_dir
 
     def file_exist(self):
         if os.path.exists(self.file_dir):
@@ -28,7 +39,7 @@ class File:
                 return fl.read()
         else:
             open(self.file_dir, 'w', encoding='utf-8')
-            return ()
+            return ''
 
     def read(self):
         return self.file
@@ -38,15 +49,12 @@ class File:
         with open(self.file_dir, 'w', encoding='utf-8') as fl:
             fl.write(self.file)
 
+    def create_tmp(self):
+        tf = tempfile.NamedTemporaryFile(dir=os.path.join(tempfile.gettempdir()))
+        self.file_dir = os.path.join(tf.name)
 
 
-
-
-
-
-
-
-path_to_file = "some_filename"
+"""path_to_file = "some_filename"
 print(os.path.exists(path_to_file))
 file_obj = File(path_to_file)
 print(os.path.exists(path_to_file))
@@ -57,13 +65,16 @@ file_obj.write("other_text")
 print(file_obj.read())
 file_obj_1 = File(path_to_file + '1')
 file_obj_2 = File(path_to_file + "2")
-file_obj_1.write("line 1 \n")
-file_obj_2.write("line 2 \n")
+file_obj_1.write("line 1\n")
+file_obj_2.write("line 2\n")
 new_file_obj = file_obj_1 + file_obj_2
+new_file_obj2 = file_obj_1 + file_obj_2
 print(isinstance(new_file_obj, File))
+print(new_file_obj)
+print(new_file_obj2)
 print(tempfile.gettempdir())
 for line in new_file_obj:
-    print(ascii(line))
+    print(ascii(line))"""
 
 
 

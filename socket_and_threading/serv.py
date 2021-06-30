836bd4090ad3492eeefd87b9187fc9f4cfe51253
C9RRY <66624951+C9RRY@users.noreply.github.com>
2021-06-30 09:14:05 +0300

    Initial commit

--- socket_and_threading/serv.py
+++ socket_and_threading/serv.py
@@ -1,14 +1,24 @@
 import socket
 
 with socket.socket() as serv_sock:
-    serv_sock.bind(("127.0.0.2", 10002))
+    serv_sock.bind(("0.0.0.0", 10002))
     serv_sock.listen(1)
 
     while True:
         conn, addr = serv_sock.accept()
+        print(addr)
         with conn:
             while True:
                 data = conn.recv(1024)
                 if not data:
                     break
+                if data.decode("utf8") == "!":
+                    break
+                conn.send(data.upper())
+                conn.send("accepted".encode("utf8"))
                 print(data.decode("utf8"))
+                print("1")
+            print("2")
+        print("3")
+
+
